7591f4fe5d15f72d84eb4dfc41a31c2f6f2c776d
Tiago Dinis <tiagoddinis@gmail.com>
2022-02-06 00:08:11 +0000

    [algorithms] fisher-yates inside out shuffle

--- algorithms/fisherYatesShuffle.ts
+++ algorithms/fisherYatesShuffle.ts
@@ -1,21 +1,23 @@
 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 
+// Shuffle array in-place
 function fisherYatesShuffle(arr: any[]) {
-  for (let i = arr.length - 1; i > 0; --i) {
-    let j = Math.trunc(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (let i = arr.length - 1; i > 0; i--) {
+    let randI = Math.trunc(Math.random() * (i + 1));
+    [arr[i], arr[randI]] = [arr[randI], arr[i]];
   }
 
   return arr;
 }
 
-function insideOut(arr: any[]): any[] {
-  let res: any[] = [1, 2, 3];
+// Construct shuffled copy of array
+function fisherYatesShuffleInsideOut(arr: any[]): any[] {
+  let res = Array(arr.length);
 
-  for (let i = 0; i < arr.length; ++i) {
-    let j = Math.trunc(Math.random() * (i + 1));
-    if (i !== j) res[i] = res[j];
-    res[j].push(arr[i]);
+  for (let i = 0; i < arr.length; i++) {
+    let randI = Math.trunc(Math.random() * (i + 1));
+    if (randI !== i) res[i] = res[randI];
+    res[randI] = arr[i];
   }
 
   return res;
