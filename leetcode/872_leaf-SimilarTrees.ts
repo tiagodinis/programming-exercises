9de86276c1be066d3fdcbf2ce93670c2bf53a1be
Tiago Dinis <tiagoddinis@gmail.com>
2022-02-19 01:12:20 +0000

    [leetcode] leaf-SimilarTrees early return version

--- leetcode/872_leaf-SimilarTrees.ts
+++ leetcode/872_leaf-SimilarTrees.ts
@@ -2,6 +2,7 @@
 
 import { btNode } from "../utilities/binaryTree";
 
+// Naive
 function leafSimilar(root1: btNode, root2: btNode): boolean {
   const leafs1 = getLeafSequence(root1);
   const leafs2 = getLeafSequence(root2);
@@ -32,4 +33,46 @@ function getLeafSequence(root: btNode): number[] {
   }
 
   return res;
+}
+
+// Early return
+function leafSimilarEarly(root1: btNode, root2: btNode): boolean {
+  let getNextLeaf1 = createGetNextLeaf(root1);
+  let getNextLeaf2 = createGetNextLeaf(root2);
+  let leaf1 = getNextLeaf1();
+  let leaf2 = getNextLeaf2();
+  while (leaf1 && leaf2) {
+    if (leaf1.val !== leaf2.val) return false;
+    leaf1 = getNextLeaf1();
+    leaf2 = getNextLeaf2();
+  }
+
+  return !leaf1 && !leaf2;
+}
+
+function createGetNextLeaf(root: btNode) {
+  let stack: btNode[] = [];
+  let current = root;
+  function getNextLeaf(): btNode {
+    let leaf = null;
+
+    while (current || stack.length) {
+      while (current) {
+        stack.push(current);
+        current = current.left;
+      }
+
+      current = stack.pop()!;
+      if (current.left === null && current.right === null) {
+        leaf = current;
+        current = current.right;
+        return leaf;
+      }
+
+      current = current.right;
+    }
+
+    return leaf;
+  }
+  return getNextLeaf;
 }
