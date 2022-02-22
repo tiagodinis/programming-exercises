db0ee6e318624bc6fddbbd9804f999dd0a3d85de
Tiago Dinis <tiagoddinis@gmail.com>
2022-02-22 18:44:13 +0000

    [leetcode] more elegant version of validateBinarySearchTree

--- leetcode/98_validateBinarySearchTree.ts
+++ leetcode/98_validateBinarySearchTree.ts
@@ -2,16 +2,30 @@
 
 import { btNode } from "../utilities/binaryTree";
 
+// Second
+function isValidBST2(root: btNode): boolean {
+  return helper2(root, -Infinity, Infinity);
+}
+
+function helper2(root: btNode, min: number, max: number): boolean {
+  if (!root) return true;
+  if (root.val >= max || root.val <= min) return false;
+  return (
+    helper2(root.left, min, root.val) && helper2(root.right, root.val, max)
+  );
+}
+
+// First
 function isValidBST(root: btNode): boolean {
   return (
-    isValidBSTWithPath(root!.left, [{ val: root!.val, left: true }]) &&
-    isValidBSTWithPath(root!.right, [{ val: root!.val, left: false }])
+    helper(root!.left, [{ val: root!.val, left: true }]) &&
+    helper(root!.right, [{ val: root!.val, left: false }])
   );
 }
 
 type pathElem = { val: number; left: boolean };
 
-function isValidBSTWithPath(root: btNode, path: pathElem[]): boolean {
+function helper(root: btNode, path: pathElem[]): boolean {
   if (!root) return true;
 
   for (let i = 0; i < path.length; i++) {
@@ -20,7 +34,7 @@ function isValidBSTWithPath(root: btNode, path: pathElem[]): boolean {
   }
 
   return (
-    isValidBSTWithPath(root.left, [{ val: root.val, left: true }, ...path]) &&
-    isValidBSTWithPath(root.right, [{ val: root.val, left: false }, ...path])
+    helper(root.left, [{ val: root.val, left: true }, ...path]) &&
+    helper(root.right, [{ val: root.val, left: false }, ...path])
   );
 }
