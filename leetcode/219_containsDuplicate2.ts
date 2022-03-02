79ba1064de9698f74ff8cfc1a15947f1eb60853c
Tiago Dinis <tiagoddinis@gmail.com>
2022-03-02 20:12:34 +0000

    [leetcode] containsDuplicate2 improvement

--- leetcode/219_containsDuplicate2.ts
+++ leetcode/219_containsDuplicate2.ts
@@ -3,10 +3,8 @@
 function containsDuplicate2(nums: number[], k: number): boolean {
   const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    if (map.has(nums[i])) {
-      if (i - map.get(nums[i]) > k) map.set(nums[i], i);
-      else return true;
-    } else map.set(nums[i], i);
+    if (map.has(nums[i]) && i - map.get(nums[i]) <= k) return true;
+    map.set(nums[i], i);
   }
   return false;
 }
