c08c252352a16613d2ab58aa28e268dbc6de90c8
Tiago Dinis <tiagoddinis@gmail.com>
2022-03-23 13:23:16 +0000

    [leetcode] cleanup oddEvenLinkedList

--- leetcode/328_oddEvenLinkedList.ts
+++ leetcode/328_oddEvenLinkedList.ts
@@ -4,20 +4,19 @@ import { lNode } from "../utilities/linkedList";
 
 function oddEvenList(head: lNode): lNode {
   if (!head) return null;
-  if (!head.next) return head;
 
   let odd: lNode = head;
   let even: lNode = head.next;
+  let evenHead: lNode = even;
 
-  while (even.next) {
-    const headEven = odd.next;
+  while (even?.next) {
     odd.next = even.next;
-    even.next = even.next!.next;
-    odd.next!.next = headEven;
-
-    if (odd.next) odd = odd.next;
-    if (even.next) even = even.next;
+    odd = odd.next;
+    even.next = even.next.next;
+    even = even.next;
   }
+
+  odd.next = evenHead;
 
   return head;
 }
